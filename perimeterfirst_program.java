51190d8ec7eb7e96cf7d1bbbe782b7552b44cb52
bpnsingh <bipin.csit.etc@gmail.com>
2023-05-03 01:26:19 +0530

    operators in java

---
+++ perimeterfirst_program.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class perimeterfirst_program {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+//        int length=10, width=20;
+//        int perimeter = 2 *(length + width);
+//        System.out.println(perimeter+" is perimerte");
+
+        // divison opertor
+        int ans = 3/4;
+        double ans1 = 3.1/2;
+        System.out.println(ans);
+        System.out.println(ans1);
+    }
+}
